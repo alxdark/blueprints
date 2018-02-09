18459227ebcbdd6d056dcdd3ef09829fd4753ca7
Alx Dark <alxdark@gmail.com>
2018-02-08 20:30:17 -0800

    Bump date

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ window.addEventListener("load", function() {
             textValue: null,
             items: data,
             categories: c,
-            date: "29 Jan 2018"
+            date: "8 Feb 2018"
         },
         created: function() {
             var arr = document.location.hash.split("=");
